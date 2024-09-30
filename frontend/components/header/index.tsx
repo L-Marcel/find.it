0556a9eb06c3a958e1d848481a57c2beabd90a10
L-Marcel <lmgh1312@gmail.com>
2024-09-30 08:27:34 -0300

    Adicionando input

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -2,7 +2,7 @@ import "./index.scss";
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <h1>Example</h1>
       <p>Of a header</p>
     </header>
