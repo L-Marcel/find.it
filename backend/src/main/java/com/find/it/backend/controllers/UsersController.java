a0b9776d26fee6273c20919d16763d07deb48711
Antonio Henrique <henrique1508cops@gmail.com>
2024-10-01 10:02:03 -0300

    ExceptionsController e UserService

--- backend/src/main/java/com/find/it/backend/controllers/UsersController.java
+++ backend/src/main/java/com/find/it/backend/controllers/UsersController.java
@@ -1,51 +1,45 @@
 package com.find.it.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.find.it.backend.dtos.UserDTO;
-import com.find.it.backend.errors.NotFound;
 import com.find.it.backend.models.User;
-import com.find.it.backend.repositories.UserRepository;
+import com.find.it.backend.services.UserService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Optional;
 import java.util.UUID;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
   @Autowired
-  private UserRepository repository;
+  private UserService service;
 
   @GetMapping
   public ResponseEntity<List<User>> getAllUsers() {
-      List<User> allUsers = repository.findAll();
+      List<User> allUsers = service.findAll();
       return ResponseEntity.ok(allUsers);
   }
   
-
   @GetMapping("/{id}")
   public ResponseEntity<User> getUserById(@PathVariable UUID id) {
-    Optional<User> user = repository.findById(id);
-    if(user.isPresent()){
-      return ResponseEntity.ok(user.get());
-    }
-    throw new NotFound("User not found");
+    User user = service.findById(id);
+    return ResponseEntity.ok(user);
   }
 
   @PostMapping
-  public ResponseEntity<String> registerUser(@RequestBody UserDTO user) {
-    User newUser = new User(user);
-    repository.save(newUser);
-    return ResponseEntity.ok("New user registered");
+  public ResponseEntity<String> registerUser(@RequestBody UserDTO newUser) {
+    service.validateAndCreateUser(newUser);
+    return ResponseEntity.status(HttpStatus.CREATED).body("New user registered");
   }
   
 }
--- backend/src/main/java/com/find/it/backend/errors/AlreadyExists.java
+++ backend/src/main/java/com/find/it/backend/errors/AlreadyExists.java
@@ -1,11 +1,22 @@
 package com.find.it.backend.errors;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import lombok.Getter;
+
 import org.springframework.http.HttpStatus;
 
+@Getter
 @ResponseStatus(HttpStatus.CONFLICT)
 public class AlreadyExists extends RuntimeException {
+  private String field;
+
   public AlreadyExists(String message) {
     super(message);
   };
+
+  public AlreadyExists(String message, String field) {
+    super(message);
+    this.field = field;
+  }
 };
