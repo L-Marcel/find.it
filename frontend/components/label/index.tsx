6bef1b4d6ff8a3871b0d8b18331593fb5c20b7e3
Antonio Henrique <henrique1508cops@gmail.com>
2024-10-19 20:06:46 -0300

    Componente Label adicionado

---
+++ frontend/components/label/index.tsx
@@ -0,0 +1,27 @@
+import "./index.scss"
+import React from "react"
+
+interface LabelProps {
+  theme?: "default" | "small";
+  header: string;
+  children: string;
+  icon?: React.ReactNode;
+}
+
+export default function Label({
+  theme,
+  header,
+  children,
+  icon
+} : LabelProps){
+
+  return (
+    <section className={`label ${theme ? theme : "default"}`}>
+      {icon ? <span className="icon">{icon}</span> : null}
+      <div className="label-content">
+        <h1 className="header">{header}</h1>
+        <p className="content">{children}</p>
+      </div>
+    </section>
+  );
+}
