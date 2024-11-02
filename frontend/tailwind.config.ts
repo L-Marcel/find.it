a1b7776b5b524a92ae23fc52a447fc9869f7e076
l-marcel <lmgh1312@gmail.com>
2024-11-02 16:30:54 -0300

    Estilizando barra de rolagem.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,35 +2,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   mode: "jit",
   theme: {
-  	extend: {
-  		fontFamily: {
-  			roboto: 'var(--font-roboto)',
-  			inter: 'var(--font-inter)',
-  			tilt: 'var(--font-tilt-warp)'
-  		},
-  		variants: {
-  			textColor: ["group-focus"]
-  		},
-  		screens: {
-  			xs: '420px'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        roboto: "var(--font-roboto)",
+        inter: "var(--font-inter)",
+        tilt: "var(--font-tilt-warp)",
+      },
+      variants: {
+        textColor: ["group-focus"],
+      },
+      screens: {
+        xs: "420px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
-  plugins: [require("tailwind-scrollbar"), require("tailwindcss-animate")],
+  plugins: [
+    require("tailwind-scrollbar"),
+    require("tailwindcss-animate"),
+    function ({ addVariant }: unknown) {
+      addVariant("firefox", ":-moz-any(&)");
+    },
+  ],
 };
 
 export default config;
